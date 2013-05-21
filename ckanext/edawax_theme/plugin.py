9985db5ce69dd48e543c0eb6194281a6bdd65299
Hendrik Bunke <h.bunke@zbw.eu>
2013-05-21 17:58:52 +0200

    implemented logos and replaced 'organizations' with 'journals'

--- ckanext/edawax_theme/plugin.py
+++ ckanext/edawax_theme/plugin.py
@@ -14,7 +14,10 @@ class EdawaxThemePlugin(plugins.SingletonPlugin, ):
 
     '''
     plugins.implements(plugins.IConfigurer)
-    plugins.implements(plugins.ITemplateHelpers)
+    #plugins.implements(plugins.ITemplateHelpers)
+    #plugins.implements(plugins.IPackageController, inherit=True)
+    plugins.implements(plugins.IFacets, inherit=True)
+    plugins.implements(plugins.IRoutes, inherit=True)
 
     # Update CKAN's config settings, see the IConfigurer plugin interface.
     def update_config(self, config):
@@ -27,5 +30,87 @@ class EdawaxThemePlugin(plugins.SingletonPlugin, ):
 
     # Tell CKAN what custom template helper functions this plugin provides,
     # see the ITemplateHelpers plugin interface.
-    def get_helpers(self):
-        return {}
+    #def get_helpers(self):
+    #    return {}
+    def before_map(self, map):
+        """
+        replacing all organization urls with journal
+        """
+
+        map.connect('organizations_index', '/journals', action='index',
+                controller='organization')
+        map.connect('/journals/list', action='list',
+        controller="organization")
+        map.connect('/journals/new', action='new', controller="organization")
+        map.connect('/journals/{action}/{id}',
+          requirements=dict(action='|'.join([
+          'edit',
+          'delete',
+          'admins',
+          'members',
+          'member_new',
+          'member_delete',
+          'history',
+          'bulk_process',
+          'about',
+          ])), controller="organization"
+          )
+        map.connect('organization_activity', '/journals/activity/{id}',
+                  action='activity', ckan_icon='time',
+                  controller="organization")
+        map.connect('organization_about', '/journals/about/{id}',
+                  action='about', ckan_icon='info-sign', controller="organization")
+        map.connect('organization_read', '/journals/{id}', action='read',
+                  ckan_icon='sitemap', controller="organization")
+        
+        #TODO redirects are just temporary, since there are still some routes
+        #and links with 'organizations' in ckan. It even might be easier, to
+        #simply redirect any organization url and leave the above maps out...
+        map.redirect('/organization', '/journals')
+        map.redirect('/organization/{url:.*}', '/journals/{url}')
+
+
+        return map
+
+
+       #map.connect('stats', '/stats',
+       #    controller='ckanext.stats.controller:StatsController',
+       #    action='index')
+       #map.connect('stats_action', '/stats/{action}',
+       #    controller='ckanext.stats.controller:StatsController')
+       #return map
+
+       
+
+
+    def organization_facets(self, facets_dict,  organization_type, package_type):
+        """
+        modify facets. We remove groups, tags and rename 'organizations'
+        'Journals'
+        """
+        facets = self.__edawax_facets(facets_dict)
+        return facets
+
+    def dataset_facets(self, facets_dict, package_type):
+        ''' Update the facets_dict and return it. we remove groups, tags and
+        rename Organizations
+        '''
+        facets = self.__edawax_facets(facets_dict)
+        return facets
+
+    
+    def __edawax_facets(self, facets_dict):
+        """
+        helper method for common facet cleaning
+        """
+        
+        try: 
+            del facets_dict['groups']
+            del facets_dict['tags']
+            facets_dict['organization'] = 'Journals'
+        except:
+            pass
+        
+        return facets_dict
+
+
