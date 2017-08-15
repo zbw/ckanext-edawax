a533f64a6a6f28233877c8fb870758a2ab7d8cbf
Hendrik Bunke <h.bunke@zbw.eu>
2017-08-15 15:57:40 +0200

    new manuals, and info leaflet

--- ckanext/edawax/plugin.py
+++ ckanext/edawax/plugin.py
@@ -230,8 +230,6 @@ class EdawaxPlugin(plugins.SingletonPlugin,):
         map.redirect('/organization/{url:.*}', '/journals/{url}')
         map.redirect('/dashboard/organizations', '/dashboard/journals')
 
-        map.redirect('/manual_en', '/User-Manual-EN-2016-07-25.pdf')
-        map.redirect('/manual_de', '/User-Manual-DE-25-07-2016.pdf')
 
         # review mail to editor
         map.connect('/dataset/{id}/review',
