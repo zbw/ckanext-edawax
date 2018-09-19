5dd77285e2df6e20f23e918ef41f27717a2ef46b
franz osorio <f.osorio@zbw.eu>
2018-09-19 16:17:47 +0200

    Move some mailing functions to the extension
    This should keep the core CKAN code clean

--- ckanext/edawax/new_invites.py
+++ ckanext/edawax/new_invites.py
@@ -8,8 +8,21 @@ be made about which template to use of the email.
 """
 import ckan, random
 from pylons import config
-import ckan.lib.mailer as mailer, ckan.logic.action.create as logic
+import ckan.lib.mailer as mailer
+import ckan.logic.action.create as logic
 from ckan.lib.base import render_jinja2
+
+#imports for expanded mailing
+from ckan.common import _, g
+from email.mime.text import MIMEText
+from email.header import Header
+from email import Utils
+from time import time
+import smtplib
+import logging
+import paste.deploy.converters
+
+log = logging.getLogger(__name__)
 
 def get_invite_body(user, data=None):
     extra_vars = {'reset_link': mailer.get_reset_link(user),
@@ -27,7 +40,9 @@ def send_invite(user, data):
     mailer.create_reset_key(user)
     body = get_invite_body(user, data=data)
     subject = mailer._('Invite for {site_title}').format(site_title=mailer.g.site_title)
-    mailer.mail_user(user, subject, body)
+    # pass role to mail function to check if the attachement should be sent
+    role = data['role']
+    mail_user(user, subject, body, {}, role)
 
 
 def user_invite(context, data_dict):
@@ -55,3 +70,81 @@ def user_invite(context, data_dict):
     logic._get_action('group_member_create')(context, member_dict)
     send_invite(user, data)
     return logic.model_dictize.user_dictize(user, context)
+
+
+# modifying CKAN's base mail function to allow sending attachments
+def _mail_recipient(recipient_name, recipient_email,
+        sender_name, sender_url, subject,
+        body, headers={}):
+    mail_from = config.get('smtp.mail_from')
+    body = mailer.add_msg_niceties(recipient_name, body, sender_name, sender_url)
+    msg = MIMEText(body.encode('utf-8'), 'plain', 'utf-8')
+    for k, v in headers.items(): msg[k] = v
+    subject = Header(subject.encode('utf-8'), 'utf-8')
+    msg['Subject'] = subject
+    msg['From'] = _("%s <%s>") % (sender_name, mail_from)
+    recipient = u"%s <%s>" % (recipient_name, recipient_email)
+    msg['To'] = Header(recipient, 'utf-8')
+    msg['Date'] = Utils.formatdate(time())
+    msg['X-Mailer'] = "CKAN %s" % ckan.__version__
+
+    # Send the email using Python's smtplib.
+    smtp_connection = smtplib.SMTP()
+    if 'smtp.test_server' in config:
+        # If 'smtp.test_server' is configured we assume we're running tests,
+        # and don't use the smtp.server, starttls, user, password etc. options.
+        smtp_server = config['smtp.test_server']
+        smtp_starttls = False
+        smtp_user = None
+        smtp_password = None
+    else:
+        smtp_server = config.get('smtp.server', 'localhost')
+        smtp_starttls = paste.deploy.converters.asbool(
+                config.get('smtp.starttls'))
+        smtp_user = config.get('smtp.user')
+        smtp_password = config.get('smtp.password')
+    smtp_connection.connect(smtp_server)
+    try:
+        #smtp_connection.set_debuglevel(True)
+
+        # Identify ourselves and prompt the server for supported features.
+        smtp_connection.ehlo()
+
+        # If 'smtp.starttls' is on in CKAN config, try to put the SMTP
+        # connection into TLS mode.
+        if smtp_starttls:
+            if smtp_connection.has_extn('STARTTLS'):
+                smtp_connection.starttls()
+                # Re-identify ourselves over TLS connection.
+                smtp_connection.ehlo()
+            else:
+                raise MailerException("SMTP server does not support STARTTLS")
+
+        # If 'smtp.user' is in CKAN config, try to login to SMTP server.
+        if smtp_user:
+            assert smtp_password, ("If smtp.user is configured then "
+                    "smtp.password must be configured as well.")
+            smtp_connection.login(smtp_user, smtp_password)
+
+        smtp_connection.sendmail(mail_from, [recipient_email], msg.as_string())
+        log.info("Sent email to {0}".format(recipient_email))
+
+    except smtplib.SMTPException, e:
+        msg = '%r' % e
+        log.exception(msg)
+        raise MailerException(msg)
+    finally:
+        smtp_connection.quit()
+
+def mail_recipient(recipient_name, recipient_email, subject,
+        body, headers={}, role=None):
+    if role is not None:
+        pass
+    return _mail_recipient(recipient_name, recipient_email,
+            g.site_title, g.site_url, subject, body, headers=headers)
+
+def mail_user(recipient, subject, body, headers={}, role=None):
+    if (recipient.email is None) or not len(recipient.email):
+        raise MailerException(_("No recipient email address available!"))
+    mail_recipient(recipient.display_name, recipient.email, subject,
+            body, headers=headers, role=role)
