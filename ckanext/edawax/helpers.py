dd46f00fe2f75f91c08d33ee55df86f1937f27f5
franz osorio <f.osorio@zbw.eu>
2019-09-09 09:47:42 +0200

    Fix multiple exceptions

--- ckanext/edawax/helpers.py
+++ ckanext/edawax/helpers.py
@@ -34,14 +34,14 @@ def get_page_type():
         pkg = None
 
     try:
-        if 'organization' in pkg.keys() and pkg['organization'] is not None:
+        if 'organization' in pkg.keys():
             journal = pkg['organization']['title']
             parent = pkg['organization']
             resource = pkg
         else:
             parent = None
             resource = None
-    except [AttributeError, TypeError]:
+    except (AttributeError, TypeError):
         parent = None
     resource = pkg
 
