8bf8584e54150da0b95d2d5b3aa23876da28319d
franz osorio <f.osorio@zbw.eu>
2020-07-28 13:30:00 +0200

    Prevent admins from seeing send to review button
    When a dataset is private and not sent to review

--- ckanext/edawax/helpers.py
+++ ckanext/edawax/helpers.py
@@ -466,9 +466,9 @@ def show_change_reviewer(pkg):
 
 def show_review_button(pkg):
     return (get_user_id() == pkg['creator_user_id'] \
-        and in_review(pkg) in ['false', 'reauthor']) or ( (has_hammer() \
-            or is_admin(pkg)) and not in_review(pkg) in ['reviewers', 'reviewed'])
-#or (has_hammer() and not in_review(pkg) in ['reviewers', 'reviewed']) and not pkg.get('private', True) or (is_admin(pkg) and in_review(pkg) in ('reauthor', 'finished', 'editor'))
+        and in_review(pkg) in ['false', 'reauthor']) or ( (has_hammer()) \
+            and not in_review(pkg) in ['reviewers', 'reviewed'])
+
 
 
 def show_manage_button(pkg):
